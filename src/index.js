1404799b70031a90c7877563f78929dd96dbe4c4
Ananya Santra <asantra@ideatarmac.com>
2019-03-30 13:24:48 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+// $(document).ready(function(){
+//     $(body).append('<div>okl</div>')
+//     // $.get('/show', function(data){
+        
+//     //     $('body').append('<i>'+data+'from server</i>');
+//     // });
+// });
+
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (<div>hello react</div>)
+ReactDOM.render(<App /> , document.getElementById('root'));
